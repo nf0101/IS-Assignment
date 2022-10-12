8dd2e3584872fa0981eb4eac46afa1596d3ddd28
Nicola Frugieri <frugierinicola.ragg@gmail.com>
2022-10-12 11:00:35 +0200

    Test

---
+++ IS-Assignment/src/main/java/Calcolatrice.java
@@ -0,0 +1,18 @@
+public class Calcolatrice {
+
+    public double somma(double a, double b){
+        return a+b;
+    }
+
+    public double sottrai(double a, double b){
+        return a-b;
+    }
+
+    public double moltiplica(double a, double b){
+        return a*b;
+    }
+
+    public double dividi(double a, double b){
+        return a/b;
+    }
+}
